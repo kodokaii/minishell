2e89e7da385d6e65f9ec4af6a3c97f1ed942bdb7
Nils Laeremans <nlaerema@c1r6p5.42lehavre.fr>
2023-12-14 15:39:31 +0100

    check error

--- parsing/token_error.c
+++ parsing/token_error.c
@@ -6,17 +6,21 @@
 /*   By: nlaerema <nlaerema@student.42lehavre.fr>	+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:58:17 by nlaerema          #+#    #+#             */
-/*   Updated: 2023/12/14 03:16:53 by nlaerema         ###   ########.fr       */
+/*   Updated: 2023/12/14 15:37:25 by nlaerema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-static t_bool	_is_error(t_token *token, t_token_type last_type)
+static t_bool	_is_error(t_list *token_list, t_token_type last_type)
 {
+	t_token	*token;
+
+	token = token_list->data;
 	if (token->type == TOKEN_ERROR)
 		return (FT_TRUE);
-	if (is_control_type(token->type) && is_control_type(last_type))
+	if (is_control_type(token->type)
+		&& (is_control_type(last_type) || !token_list->next))
 		return (FT_TRUE);
 	return (FT_FALSE);
 }
@@ -28,7 +32,7 @@ t_bool	token_error(t_list	*token_list)
 	last_type = TOKEN_PIPE;
 	while (token_list)
 	{
-		if (_is_error(token_list->data, last_type))
+		if (_is_error(token_list, last_type))
 		{
 			syntax_error(token_list->data);
 			return (FT_TRUE);
