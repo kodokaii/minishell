f83744410815585ef6b84f082ece19bd60f2f41f
demostanis <demostanis@protonmail.com>
2023-12-15 17:22:03 +0100

    add redirection tests

---
+++ tests/test_redirection.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_redirection.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgodard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 0000/00/00 00:00:00 by cgodard           #+#    #+#             */
+/*   Updated: 2023/12/15 04:24:06 by cgodard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <assert.h>
+#include "libtest.h"
+
+int	main(void)
+{
+	ENSURE_OUTPUT(
+		ARGV({"./minishell", "rm -f /tmp/minishell_test &&"
+		   "echo hello > /tmp/minishell_test && cat /tmp/minishell_test", NULL}),
+		"hello");
+
+	ENSURE_OUTPUT(
+		ARGV({"./minishell", "rm -f /tmp/minishell_test &&"
+			"echo hello > /tmp/minishell_test && cat < /tmp/minishell_test", NULL}),
+		"hello");
+
+	ENSURE_OUTPUT(
+		ARGV({"./minishell", "rm -f /tmp/minishell_test &&"
+			"echo hello > /tmp/none > /tmp/minishell_test && cat /tmp/none /tmp/minishell_test", NULL}),
+		"hello");
+
+	ENSURE_OUTPUT(
+		ARGV({"./minishell", "rm -f /tmp/minishell_test &&"
+			"echo hello > /tmp/minishell_test | cat", NULL}),
+		NULL);
+}
