1cf1de9b7c58b6bf38e81746b3c4bc212ee0e9f6
demostanis <demostanis@protonmail.com>
2023-12-15 03:03:42 +0100

    add ft_last_exit_code()

--- execution_utils.c
+++ execution_utils.c
@@ -6,11 +6,21 @@
 /*   By: nlaerema <nlaerema@student.42lehavre.fr>	+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:58:17 by nlaerema          #+#    #+#             */
-/*   Updated: 2023/12/15 02:41:08 by nlaerema         ###   ########.fr       */
+/*   Updated: 2023/12/15 03:03:21 by cgodard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*ft_last_exit_code(char *exit_code)
+{
+	static char	*last_exit_code = 0;
+
+	if (exit_code == NULL)
+		return (last_exit_code);
+	last_exit_code = exit_code;
+	return (last_exit_code);
+}
 
 void	command_not_found(t_cmd *cmd)
 {
