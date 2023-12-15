220d217d552fd6e1bad036f5123fa80eb47be712
demostanis <demostanis@protonmail.com>
2023-12-15 02:56:24 +0100

    pass environ in tests and fix open of out file

--- parsing/files.c
+++ parsing/files.c
@@ -6,7 +6,7 @@
 /*   By: cgodard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 02:14:38 by cgodard           #+#    #+#             */
-/*   Updated: 2023/12/15 02:23:50 by cgodard          ###   ########.fr       */
+/*   Updated: 2023/12/15 02:55:33 by cgodard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void	open_fds(t_token *token, t_cmd *cmd)
 {
 	if (token->type == TOKEN_IO_OUT && cmd->fd_in != INVALID_FD)
 		reporting_open(
-			token->data, O_CREAT | O_TRUNC | O_WRONLY, 0644, &cmd->fd_in);
+			token->data, O_CREAT | O_TRUNC | O_WRONLY, 0644, &cmd->fd_out);
 	else if (token->type == TOKEN_IO_IN && cmd->fd_out != INVALID_FD)
 		reporting_open(token->data, O_RDONLY, 0, &cmd->fd_in);
 	else if (token->type == TOKEN_IO_APPEND && cmd->fd_in != INVALID_FD)
